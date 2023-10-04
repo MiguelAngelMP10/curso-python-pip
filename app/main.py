d250332a32a69ac76020763306bef418c633a677
Miguel Angel Muñoz Pozos <mmunozpozos@gmail.com>
2023-10-04 08:51:53 -0600

    Pandas

--- app/main.py
+++ app/main.py
@@ -2,20 +2,32 @@ import utils
 import read_csv
 import charts
 import pandas as pd
+import utils
+import read_csv
+import charts
+import pandas as pd
 
 
 def run():
+    # Código para generar el pie chart sin usar PANDAS
     '''
-  data = list(filter(lambda item : item['Continent'] == 'South America',data))
-  countries = list(map(lambda x: x['Country'], data))
-  percentages = list(map(lambda x: x['World Population Percentage'], data))
-  '''
+    data = read_csv.read_csv('data.csv')
+    data = list(filter(lambda item : item['Continent'] == 'South America',data))
 
-    df = pd.read_csv('data.csv')
-    df = df[df['Continent'] == 'Africa']
+    countries = list(map(lambda x: x['Country'], data))
+    percentages = list(map(lambda x: x['World Population Percentage'], data))
+    charts.generate_pie_chart(countries, percentages)
+    '''
+    # #Código equivalente usando PANDAS
 
+    # df(dataframe)
+    df = pd.read_csv("data.csv")  # Nos ahorramos el método creado read_csv.py
+    df = df[df['Continent'] == 'North America']
+    # Equivalente a -> data = list(filter(lambda item : item['Continent'] == 'South America',data))
     countries = df['Country'].values
+    # Equivalente a -> countries = list(map(lambda x: x['Country'], data))
     percentages = df['World Population Percentage'].values
+    # Equivalente a -> percentages = list(map(lambda x: x['World Population Percentage'], data))
     charts.generate_pie_chart(countries, percentages)
 
     data = read_csv.read_csv('data.csv')
